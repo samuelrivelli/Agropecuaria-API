ce60a0b2efb7cc88602bb86410cf0fd230adf984
Samuel Rivelli <samuelmagrivelli@gmail.com>
2024-02-24 17:55:24 -0300

    Adição do restante das classes de serviço

--- src/main/java/com/example/agropecuariaapi/service/ClienteService.java
+++ src/main/java/com/example/agropecuariaapi/service/ClienteService.java
@@ -2,20 +2,33 @@ package com.example.agropecuariaapi.service;
 
 import com.example.agropecuariaapi.model.entity.Cliente;
 import com.example.agropecuariaapi.model.repository.ClienteRepository;
-import lombok.AllArgsConstructor;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
-@AllArgsConstructor
+@Service
 public class ClienteService {
 
     private ClienteRepository repository;
-    public List<Cliente> getClientes(){
+    public List<Cliente> findAll(){
         return repository.findAll();
     }
-    public Optional<Cliente> getClienteById(Long id){
+    public Optional<Cliente> findById(Long id){
         return repository.findById(id);
+    }
+
+    @Transactional
+    public Cliente salvar(Cliente cliente){
+        return repository.save(cliente);
+    }
+
+    @Transactional
+    public void excluir(Cliente cliente){
+        Objects.requireNonNull(cliente.getId());
+        repository.delete(cliente);
     }
 
 
