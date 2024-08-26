78baf0629ef4bb19a412e1c35e6d9fa5c51d2cfb
Samuel Rivelli <samuelmagrivelli@gmail.com>
2024-08-25 23:02:33 -0300

    Correcao do findbyid de venda

--- src/main/java/com/example/agropecuariaapi/controller/VendaController.java
+++ src/main/java/com/example/agropecuariaapi/controller/VendaController.java
@@ -63,7 +63,7 @@ public class VendaController {
     })
 
     public ResponseEntity<?> findById(@PathVariable("id") Long id) {
-        Optional<Venda> venda = repository.findByIdWithProdutos(id);
+        Optional<Venda> venda = service.findById(id);
 
         if (venda.isEmpty()) {
             return new ResponseEntity<>("Venda não encontrada", HttpStatus.NOT_FOUND);
--- src/main/java/com/example/agropecuariaapi/model/repository/VendaRepository.java
+++ src/main/java/com/example/agropecuariaapi/model/repository/VendaRepository.java
@@ -2,13 +2,7 @@ package com.example.agropecuariaapi.model.repository;
 
 import com.example.agropecuariaapi.model.entity.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.nio.file.LinkOption;
-import java.util.Optional;
 
 public interface VendaRepository extends JpaRepository<Venda, Long> {
-    @Query("SELECT v FROM Venda v JOIN FETCH v.vendaProdutos WHERE v.id = :id")
-    Optional<Venda> findByIdWithProdutos(@Param("id") Long id);
+
 }
