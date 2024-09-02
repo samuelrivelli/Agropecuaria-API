7befeef077d9e239544e270c867f693724bd877f
Samuel Rivelli <samuelmagrivelli@gmail.com>
2024-09-02 19:16:19 -0300

    remocao do carrinho

--- src/main/java/com/example/agropecuariaapi/model/repository/CarrinhoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.agropecuariaapi.model.repository;
-
-import com.example.agropecuariaapi.model.entity.Carrinho;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarrinhoRepository extends JpaRepository<Carrinho, Long> {
-}
